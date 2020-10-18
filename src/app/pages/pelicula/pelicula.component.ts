7acd55b5878c438977f11e8a8b8d6271be3d1719
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-10-17 23:32:38 -0500

    261

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PeliculasService } from '../../services/peliculas.service';
+import { MovieResponse } from '../../interfaces/movie-response';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pelicula',
@@ -8,9 +10,12 @@ import { PeliculasService } from '../../services/peliculas.service';
   styleUrls: ['./pelicula.component.css']
 })
 export class PeliculaComponent implements OnInit {
+    
+  public pelicula: MovieResponse;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private peliculasService:PeliculasService) { }
+              private peliculasService:PeliculasService,
+              private location: Location ) { }
 
   ngOnInit(): void {
 
@@ -18,9 +23,11 @@ export class PeliculaComponent implements OnInit {
 
    this.peliculasService.getPeliculaDetalle(id).subscribe(movie =>{
      console.log(movie);
-     
+     this.pelicula = movie; 
    })
    
   }
-
+  onRegresar(){
+    this.location.back();
+  }
 }
