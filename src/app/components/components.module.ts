73f09e4f2a275aad36a858109bd33cc15f3fe374
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-10-17 21:51:42 -0500

    257

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { RatingModule } from 'ng-starrating';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 
 
@@ -22,7 +23,8 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
   imports: [
     CommonModule,
     RouterModule,
-    RatingModule
+    RatingModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
