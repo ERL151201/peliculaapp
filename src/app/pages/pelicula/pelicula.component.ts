aa4652468cdc0cda8f3bbd9c17c8d40c4f652cba
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-10-18 13:39:45 -0500

    264

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -4,6 +4,7 @@ import { PeliculasService } from '../../services/peliculas.service';
 import { MovieResponse } from '../../interfaces/movie-response';
 import { Location } from '@angular/common';
 import { Cast } from '../../interfaces/credits-response';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-pelicula',
@@ -24,18 +25,34 @@ export class PeliculaComponent implements OnInit {
 
    const { id } = this.activatedRoute.snapshot.params;
 
-   this.peliculasService.getPeliculaDetalle(id).subscribe(movie =>{
-     if (!movie) {
-       this.router.navigateByUrl('/home');
-       return;
-     }
-     this.pelicula = movie; 
-   });
-   
-  this.peliculasService.getCast(id).subscribe(cast =>{
-    console.log(cast);
-    this.cast = cast.filter(actor => actor.profile_path !== null);
-  });
+    combineLatest([
+      this.peliculasService.getPeliculaDetalle(id),
+      this.peliculasService.getCast(id)
+      
+      
+
+    ]).subscribe(([pelicula, cast]) =>{
+            if (!pelicula) {
+              this.router.navigateByUrl('/home');
+              return;
+            }
+        this.pelicula = pelicula;
+        this.cast = cast.filter(actor => actor.profile_path !== null);
+    });
+
+
+  // this.peliculasService.getPeliculaDetalle(id).subscribe(movie =>{
+  //   if (!movie) {
+  //     this.router.navigateByUrl('/home');
+  //     return;
+ //   }
+  //   this.pelicula = movie; 
+  // });
+ //  
+ // this.peliculasService.getCast(id).subscribe(cast =>{
+ //   console.log(cast);
+ //   this.cast = cast.filter(actor => actor.profile_path !== null);
+ // });
 
   }
   onRegresar(){
